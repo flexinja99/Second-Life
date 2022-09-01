ff8af3535f883b4d7f220c790340091a56fe2de5
kangDW418 <kdw418@naver.com>
2022-09-01 20:37:13 +0900

    Playerpf 2
    9/1

---
+++ platformer 2d 2nd/Assets/02.Scripts/Example/Example_StateMachineManagerForEnemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example_StateMachineManagerForEnemy : Example_StateMachineManagerBase
+{
+    //public enum StateType
+    //{
+    //    Idle,
+    //    Move,
+    //    Attack,
+    //    Hurt,
+    //    Die,
+    //}
+    //
+    //public void RefreshStates()
+    //{
+    //    var types = Enum.GetValues(typeof(StateType));
+    //    foreach (StateType type in types)
+    //    {
+    //        AddStateMachine(type);
+    //    }
+    //}
+}
---
+++ platformer 2d 2nd/Assets/02.Scripts/Example/Example_StateMachineManagerForPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example_StateMachineManager : Example_StateMachineManagerBase
+{
+    //public enum StateType
+    //{
+    //    Idle,
+    //    Move,
+    //    Attack,
+    //    Skill1,
+    //    Skill2,
+    //    Jump,
+    //    Crouch,
+    //    Dash,
+    //    Hurt,
+    //    Die,
+    //}
+    //
+    //public void RefreshStates()
+    //{
+    //    var types = Enum.GetValues(typeof(StateType));
+    //    foreach (StateType type in types)
+    //    {
+    //        AddStateMachine(type);
+    //    }
+    //}
+}
